bc500c3043f87d47c62324dc128431313dbc8046
Samnang <samnang.chhun@gmail.com>
2010-02-08 13:02:26 +0700

    Added another example of Adapter in javascript

---
+++ Adapter_JS/example_2.js
@@ -0,0 +1,25 @@
+/*
+Adapter Pattern:
+
+Convert the interface of a class into another interface clients expect.
+Adapter lets classes work together that couldn't otherwise because of incompatible interfaces.
+*/
+
+//Third party
+Calculator = function() {
+	this.sum = function(a, b) {
+		console.log('sum = ' + (a + b).toString());
+	};
+};
+
+//Adapter
+CalculatorWrapper = function() {
+	var _adaptee = new Calculator();
+	
+	this.sum = function(a, b) {
+		_adaptee.sum(a, b);
+	};
+};
+
+var calculator = new CalculatorWrapper();
+calculator.sum(1, 2);
