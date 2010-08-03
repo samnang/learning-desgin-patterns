6c374eb0aedc77a3f95fb57e6e822dc61b271dc4
Samnang <samnang.chhun@gmail.com>
2010-08-03 21:28:17 +0700

    Implemented ChainOfResponsibility pattern

---
+++ ChainOfResponsibility_CS/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace ChainOfResponsibility_CS
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var hundredCashHandler = new HundredCashHandler();
+            var fiftyCashHandler = new FiftyCashHandler();
+            var twentyCashHandler = new TwentyCashHandler();
+            var tenCashHandler = new TenCashHandler();
+
+            hundredCashHandler.AppendHandler(fiftyCashHandler);
+            hundredCashHandler.AppendHandler(twentyCashHandler);
+            hundredCashHandler.AppendHandler(tenCashHandler);
+
+            hundredCashHandler.Process(230);
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class ATMCassetteHandler
+    {
+        public ATMCassetteHandler NextHandler { get; private set; }
+
+        public bool CanHandle(double amount)
+        {
+            return amount >= AmountCanHandle;
+        }
+
+        public void Process(double amount)
+        {
+            if (CanHandle(amount))
+                Console.WriteLine("{0} money: {1}", AmountCanHandle, Math.Floor(amount/AmountCanHandle));
+
+            ProcessRemaindingAmount(amount);
+        }
+
+        private void ProcessRemaindingAmount(double amount)
+        {
+            double remainingAmount = amount%AmountCanHandle;
+            if (remainingAmount > 0)
+            {
+                if (NextHandler != null)
+                {
+                    NextHandler.Process(remainingAmount);
+                }
+                else
+                {
+                    Console.WriteLine("With small money: {0}", remainingAmount);
+                }
+            }
+        }
+
+        public void AppendHandler(ATMCassetteHandler handler)
+        {
+            if (NextHandler == null)
+            {
+                NextHandler = handler;
+            }
+            else
+            {
+                NextHandler.AppendHandler(handler);
+            }
+        }
+
+        public abstract double AmountCanHandle { get; }
+    }
+
+    public class HundredCashHandler : ATMCassetteHandler
+    {
+        public override double AmountCanHandle
+        {
+            get { return 100; }
+        }
+    }
+
+    public class FiftyCashHandler : ATMCassetteHandler
+    {
+        public override double AmountCanHandle
+        {
+            get { return 50; }
+        }
+    }
+
+    public class TwentyCashHandler : ATMCassetteHandler
+    {
+        public override double AmountCanHandle
+        {
+            get { return 20; }
+        }
+    }
+
+    public class TenCashHandler : ATMCassetteHandler
+    {
+        public override double AmountCanHandle
+        {
+            get { return 10; }
+        }
+    }
+}
---
+++ Visitor_CS/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Visitor_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var modemConfigurator = new UnixModemConfigurator();
+            var hayesModem = new HayesModem();
+            var zoomModem = new ZoomModem();
+            var ernieModem = new ErnieModem();
+
+            hayesModem.Accept(modemConfigurator);
+            Console.WriteLine("Hayes Modem Configuration: " + hayesModem.InternalConfiguration);
+
+            zoomModem.Accept(modemConfigurator);
+            Console.WriteLine("Zoom Modem Configuration: " + zoomModem.ConfigurationValue);
+
+            ernieModem.Accept(modemConfigurator);
+            Console.WriteLine("Ernie Modem Configuration: " + ernieModem.ConfigurationString);
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IModem
+    {
+        void Dial(string number);
+        void Hangup();
+        void Send(char charecter);
+        char Receive();
+        void Accept(IModemVisitor visitor);
+    }
+
+    public class ErnieModem : IModem
+    {
+        public void Dial(string number) { }
+        public void Hangup() { }
+        public void Send(char charecter) { }
+        public char Receive() { return '0'; }
+
+        public void Accept(IModemVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public string ConfigurationString { get; set; }
+    }
+
+    public class ZoomModem:IModem
+    {
+        public void Dial(string number) { }
+        public void Hangup() { }
+        public void Send(char charecter) { }
+        public char Receive() { return '0'; }
+
+        public void Accept(IModemVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public int ConfigurationValue { get; set; }
+    }
+
+    public class HayesModem:IModem
+    {
+        public void Dial(string number) { }
+        public void Hangup() { }
+        public void Send(char charecter) { }
+        public char Receive() { return '0'; }
+
+        public void Accept(IModemVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+        public string InternalConfiguration { get; set; }
+    }
+
+    public interface IModemVisitor
+    {
+        void Visit(HayesModem modem);
+        void Visit(ZoomModem modem);
+        void Visit(ErnieModem modem);
+    }
+
+    public class UnixModemConfigurator : IModemVisitor
+    {
+        public void Visit(ErnieModem modem)
+        {
+            modem.ConfigurationString = "C is too slow";
+        }
+
+        public void Visit(ZoomModem modem)
+        {
+            modem.ConfigurationValue = 42;
+        }
+
+        public void Visit(HayesModem modem)
+        {
+            modem.InternalConfiguration = "&s1=4&D=3";
+        }
+    }
+}
